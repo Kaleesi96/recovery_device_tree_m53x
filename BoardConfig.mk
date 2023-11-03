3ff4842c91426c033c432630ccac513dac800801
Kaleesi <131565824+Kaleesi96@users.noreply.github.com>
2023-11-03 02:09:35 -0300

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,44 @@
+DEVICE_PATH := device/zto/m53x
+
+# For building with TWRP minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture (arm)                         # Architecture (arm64)
+TARGET_ARCH := arm                           TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv7-a-neon          TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a          OR    TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi                   TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic                TARGET_CPU_VARIANT := generic
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := m53x
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 loop.max_part=7
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_KERNEL_BASE : 0x40078000
+BOARD_KERNEL_PAGESIZE : 2048
+BOARD_RAMDISK_OFFSET : 0x11088000
+BOARD_KERNEL_TAGS_OFFSET : 0x07c08000
+BOARD_FLASH_BLOCK_SIZE : 131072 ? (BOARD_KERNEL_PAGESIZE ? 64)
+BOARD_MKBOOTIMG_ARGS +-disk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS +- --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6877
+
+PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true #Only if you need it
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
