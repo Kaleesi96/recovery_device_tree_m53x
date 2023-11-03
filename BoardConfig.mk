2feb60ef3dd09c50b2df0d3ab19ed2894fd051ea
Kaleesi <131565824+Kaleesi96@users.noreply.github.com>
2023-11-03 03:21:05 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,6 @@ PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true #Only if you need it
+TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
