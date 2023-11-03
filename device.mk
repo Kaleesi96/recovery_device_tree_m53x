02ba2f852ef34995e192e7268e87ba7e978b38bc
Kaleesi <131565824+Kaleesi96@users.noreply.github.com>
2023-11-03 03:22:50 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,1 +1,20 @@
 LOCAL_PATH := device/zto/m53x
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+	android.hardware.fastboot@1.1-impl-mock \
+	fastbootd
+
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.fastbootd.available=true
+
+# USB
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.sys.usb.storage.type=mtp
+
+# Kernel
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/Image:kernel
